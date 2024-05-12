690c986f1c9069513edec2ff0973fcdcf1e6b7e4
savavukmirovic <savavukmirovic@gmail.com>
2024-05-12 19:40:44 +0200

    Update Django Admin

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users."""
     def create_user(self, email, password=None, **extra_fields):
-        """Create save and return new user."""
+        """Create, save and return new user."""
         if not email:
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -32,7 +32,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """User in system."""
+    """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
