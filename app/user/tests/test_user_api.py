17803badb13d970de500cf8e429b567a64e34c02
savavukmirovic <savavukmirovic@gmail.com>
2024-05-26 23:36:45 +0200

    Update Recipe app

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -92,7 +92,6 @@ class PublicUserApiTest(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-
     def test_create_token_email_not_found(self):
         """Test error returned if user not found for given email."""
         payload = {'email': 'test@example.com', 'password': 'pass123'}
